b79353a8a6537d878401ae6ebf8c463543e8878f
PIASH <zmnobab1999@gmail.com>
2025-11-24 14:32:14 +0600

    introduction to the path module

---
+++ output/path-basic.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+console.log("current file info: \n");
+console.log("filename: ", __filename);
+console.log("Directory: ", __dirname);
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const filePath = "/piash/documents/nextLevel.pdf";
+
+console.log("analyzing Path :", filePath, "\n");
+console.log("Directory :", path.dirname(filePath));
+console.log("Base Name :", path.basename(filePath));
+console.log("File Extension :", path.extname(filePath));
+console.log("File Name :", path.basename(filePath, path.extname(filePath)));
+
+console.log("\n" + "-".repeat(50) + "\n");
+
+const parsed = path.parse(filePath);
+console.log("Parsed path object :", parsed);
+
+console.log("formatted path :", path.format(parsed))
